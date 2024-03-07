b7478945536e1676e1cf7f1d1a7c6c3356bf2862
Anara <berdali.aygerim9@gmail.com>
2024-03-07 20:26:59 +0000

    tests passed

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,20 +1,29 @@
-import React from "react"
-
-function Character({name, imgUrl, birth, death, race, realm, spouse}) {
+const Character = ({ name, imgUrl, birth, death, race, realm, spouse }) => {
   return (
-  <div>
-  <h2>{name}</h2>
-  <img src={imgUrl} alt={`Image of ${name}`} />
-  <ul>
-    <li>Date of Birth:{birth} </li>
-    <li>Date of Death: {death} </li>
-    <li>Race:{race}</li>
-    <li>Realm:{realm} </li>
-    <li>Spouse: {spouse}</li>
-  </ul>
-  </div>
+      <div>
+          <h2>{ name }</h2>
 
-  )
-}
+          <img src={ imgUrl } alt={ name } />
 
-export default Character
+          <ul>
+              <li>
+                  Date of Birth: { birth }
+              </li>
+              <li>
+                  Date of Death: { death }
+              </li>
+              <li>
+                  Race: { race }
+              </li>
+              <li>
+                  Realm: { realm }
+              </li>
+              <li>
+                  Spouse: { spouse }
+              </li>
+          </ul>
+      </div>
+  );
+};
+
+export default Character;
--- src/components/CharacterGallery.jsx
+++ src/components/CharacterGallery.jsx
@@ -1,20 +1,13 @@
-import React from "react";
-import characterData from "../data/characterData.json";
-import Character from "./Character";
+import characterData from '../data/characterData.json'
+import Character from './Character'
 
-function CharacterGallery() {
-  return characterData.map((data) => (
-    <Character
-      key={data._id}
-      name={data.name}
-      imgUrl={data.imgUrl}
-      birth={data.birth}
-      death={data.death}
-      race={data.race}
-      realm={data.realm}
-      spouse={data.spouse}
-    />
-  ));
-}
+const CharacterGallery = () => {
+    const listOfChars = characterData.map((char, i) => <Character { ...char } key={ i } />)
+    return (
+        <section>
+            { listOfChars }
+        </section>
+    );
+};
 
 export default CharacterGallery;
